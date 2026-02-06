353eaf841cfee41f1f3595d610a3b057f30767f1
Gustavo Luiz Gregorio <gustavo.l.gregorio@gmail.com>
2026-02-06 14:43:27 -0300

    feat: update dist files

--- dist/AutoTheme.d.ts
+++ dist/AutoTheme.d.ts
@@ -6,27 +6,24 @@ type ColorOKLAB = `oklab(${number}% ${number} ${number} / ${number})`;
 type ColorOKLCH = `oklch(${number}% ${number} ${number}deg / ${number})`;
 type Color = ColorHEX | ColorRGB | ColorHSL | ColorOKLAB | ColorOKLCH;
 type ThemeProperties = "primary" | "secondary" | "tertiary" | "accent" | "neutral";
-type ShadeSpaces = "50" | "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900";
-type ShadeProperties = `${ThemeProperties}-${ShadeSpaces}`;
+type Shades = "50" | "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900";
+type ShadeSpaces = Partial<Record<Shades, Shades>>;
+type ShadeProperties = `${ThemeProperties}-${Shades}`;
 type ThemeShades = Partial<Record<ShadeProperties, Color>>;
-type Theme = Record<ThemeProperties, ThemeShades> & {
+type Theme = Record<Shades, ThemeShades> & {
     colorType: ColorTypes;
     baseColor: Color;
 };
-export declare class AutoTheme implements Theme {
+export declare class AutoTheme {
     #private;
     colorType: ColorTypes;
     baseColor: Color;
-    primary: ThemeShades;
-    secondary: ThemeShades;
-    tertiary: ThemeShades;
-    accent: ThemeShades;
-    neutral: ThemeShades;
-    constructor(color: Color, type?: ColorTypes);
-    addComplementary(): this;
-    addTriade(): this;
-    addShades(rangeStart: ShadeSpaces, rangeEnd: ShadeSpaces): this;
-    convertColorsTo(type: ColorTypes): this;
+    primary: ShadeSpaces;
+    secondary: ShadeSpaces;
+    tertiary: ShadeSpaces;
+    accent: ShadeSpaces;
+    neutral: ShadeSpaces;
+    constructor(color: Color, inputType?: ColorTypes, outputType?: ColorTypes);
     static serialize(autoThemeObject: AutoTheme, escapeChar?: string): string;
     static deserialize(serializedTheme: string, escapeChar?: string): Theme;
 }
